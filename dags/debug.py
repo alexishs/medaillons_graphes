92af49df14c724c1b4cd0502207ebf756dcb4d05
Alexis Halbot-Schoonaert <a.halbotschoonaert@gmail.com>
2025-10-22 14:39:51 +0000

    feat: :sparkles: On passe de 500000 relations à 5M

--- dags/debug.py
+++ dags/debug.py
@@ -7,21 +7,22 @@ utils.definir_en_test()
 
 def generer_csv()-> None:
     
-    liste_entiers = list(range(1,1000000))
+    liste_entiers = list(range(1,1_000_000))
     with open(utils.chemin_base(f"{utils.REPERTOIRE_DONNEES_BRUTES}/noeuds.csv"), mode='w', encoding='utf-8') as fichier:
         for numero in liste_entiers:
             fichier.write(f"{numero},Personne,Nom_{numero}\n")
-    random.shuffle(liste_entiers)
     with open(utils.chemin_base(f"{utils.REPERTOIRE_DONNEES_BRUTES}/relations.csv"), mode='w', encoding='utf-8') as fichier:
-        numero_precedent = 0
-        for numero in liste_entiers:
-            if numero_precedent > 0:
-                fichier.write(f"{numero_precedent},{numero},REL\n")
-                numero_precedent = 0
-            else:
-                numero_precedent = numero
+        for _ in range(10):
+            random.shuffle(liste_entiers)
+            numero_precedent = 0
+            for numero in liste_entiers:
+                if numero_precedent > 0:
+                    fichier.write(f"{numero_precedent},{numero},REL\n")
+                    numero_precedent = 0
+                else:
+                    numero_precedent = numero
 
 utils.creer_repertoires()
 generer_csv()
 traitement_bronze()
-traitement_silver()
+#traitement_silver()
